fe19f4fc4cb694cd2136a6b25efe8f4ba75b589b
Dorone <dorone.atlan@epitech.eu>
2026-09-03 01:34:26 +0200

    refactor: update account service

--- src/services/accountService.js
+++ src/services/accountService.js
@@ -1,8 +1,6 @@
 import { account } from '../data/user'
 import { mockDelay } from './mockDelay'
 
-// Aujourd'hui : données mock. Demain : `return request('/api/v1/employees/me/balance')`.
-// La signature et la forme des données renvoyées ne changent pas pour les composants.
 export async function getAccount() {
     return mockDelay(account)
 }
