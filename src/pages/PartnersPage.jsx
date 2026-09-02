924299dca225ed2500d10e8ad467f2e9045769c6
noo-noo <noah.heurteaut@epitech.eu>
2026-09-02 16:01:29 +0200

    feat(PartnerPage): add prtner page with listed partners

--- src/pages/PartnersPage.jsx
+++ src/pages/PartnersPage.jsx
@@ -1,11 +1,24 @@
 import PageHeader from '../components/ui/PageHeader'
 import '../styles/Partners.css'
+import { partners } from '../data/partners'
+
+import { Panel, PanelGroup } from 'rsuite'
 
 export default function PartnerPage() {
     return (
         <>
             <PageHeader title="Nos Partenaires" />
             <p className='partners-title'>Nous travaillons main dans la main avec des partenaires de confiance pour offrir le meilleur à nos clients. Intéressé par un partenariat ? Contactez-nous.</p>
+            <PanelGroup>
+                {partners.map((partner) => (
+                    <div className='partner-panel' key={partner.id}>
+                        <Panel header={partner.name}>
+                            <p>description: {partner.description}</p>
+                            <p>localisation: {partner.city}</p>
+                        </Panel>
+                    </div>
+                ))}
+            </PanelGroup>
         </>
     )
 }
