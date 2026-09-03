de94a005123f274cde7c2404c77f6ee3ab986c54
Albane Heraud <albane.heraud@epitech.eu>
2026-09-03 10:54:37 +0200

    feat: front qr genreation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import HomePage from './pages/HomePage'
-// import QrCodePage from './pages/QrCodePage'
+import QrCodePage from './pages/QrCodePage'
 import QrLibraryPage from './pages/QrLibraryPage'
 // import PartnersPage from './pages/PartnersPage'
 // import HistoryPage from './pages/HistoryPage'
@@ -19,7 +19,7 @@ export default function App() {
                     <Route element={<Layout />}>
                         <Route path="/" element={<HomePage />} />
                         <Route path="/mes-qr-codes" element={<QrLibraryPage />} />
-                        {/* <Route path="/qr-code" element={<QrCodePage />} /> */}
+                        <Route path="/qr-code" element={<QrCodePage />} />
                         {/* <Route path="/partenaires" element={<PartnersPage />} /> */}
                         {/* <Route path="/historique" element={<HistoryPage />} /> */}
                         {/* <Route path="/aide" element={<HelpPage />} /> */}
