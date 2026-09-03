23e24813ada6b9375a61332640a1ef6e73dded29
noo-noo <noah.heurteaut@epitech.eu>
2026-09-03 11:21:21 +0200

    feat: new logo with implemented pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import './App.css'
 import HomePage from './pages/HomePage'
 import QrCodePage from './pages/QrCodePage'
 import QrLibraryPage from './pages/QrLibraryPage'
-// import PartnersPage from './pages/PartnersPage'
-// import HistoryPage from './pages/HistoryPage'
-// import HelpPage from './pages/HelpPage'
-// import ScanSuccess from './pages/ScanSuccess'
-// import EmployeeSpace from './pages/EmployeeSpace'
+import PartnersPage from './pages/PartnersPage'
+import HistoryPage from './pages/HistoryPage'
+import HelpPage from './pages/HelpPage'
+import ScanSuccess from './pages/ScanSuccess'
+import EmployeeSpace from './pages/EmployeeSpace'
 import { QrCodesProvider } from './context/QrCodesContext'
 import Layout from './components/Layout.jsx'
 
@@ -20,12 +20,12 @@ export default function App() {
                         <Route path="/" element={<HomePage />} />
                         <Route path="/mes-qr-codes" element={<QrLibraryPage />} />
                         <Route path="/qr-code" element={<QrCodePage />} />
-                        {/* <Route path="/partenaires" element={<PartnersPage />} /> */}
-                        {/* <Route path="/historique" element={<HistoryPage />} /> */}
-                        {/* <Route path="/aide" element={<HelpPage />} /> */}
+                        <Route path="/partenaires" element={<PartnersPage />} />
+                        <Route path="/historique" element={<HistoryPage />} />
+                        <Route path="/aide" element={<HelpPage />} />
                     </Route>
-                    {/* <Route path="/scan-reussi" element={<ScanSuccess />} /> */}
-                    {/* <Route path="/espace-salarie" element={<EmployeeSpace />} /> */}
+                    <Route path="/scan-reussi" element={<ScanSuccess />} />
+                    <Route path="/espace-salarie" element={<EmployeeSpace />} />
                 </Routes>
             </BrowserRouter>
         </QrCodesProvider>
